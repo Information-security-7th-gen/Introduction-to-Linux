144cb8332e29f8afddbac3721f5eebd16879342a
k175jp <tai.sp.tai@gmail.com>
2023-06-24 08:42:21 +0000

    [add] 問題8の追加

--- script/unit_maker.sh
+++ script/unit_maker.sh
@@ -1,4 +1,4 @@
-for i in `seq 7`
+for i in `seq 8`
 do
     mkdir -p $HOME/challenge/unit$i
 done
@@ -14,3 +14,35 @@ mkdir -p $HOME/challenge/unit3/en/foobar
 touch $HOME/challenge/unit4/answer4.txt.tmp
 
 cp ./colors.txt $HOME/challenge/unit7/
+
+for i in `seq 4`
+do
+    for j in `seq 12`
+    do
+	case "$j" in
+		"2" )
+			for k in `seq 28`
+			do
+				mkdir -p $HOME/challenge/unit8/202$i/$j/$k
+			done	
+		;;
+		"1" | "3" | "5" | "7" | "8" | "10" | "12" )
+			for k in `seq 31`
+			do
+				mkdir -p $HOME/challenge/unit8/202$i/$j/$k
+			done	
+
+		;;
+		* )
+			for k in `seq 30`
+			do
+				mkdir -p $HOME/challenge/unit8/202$i/$j/$k
+			done	
+
+		;;
+	esac
+    done
+    echo "finish 202$i"
+done
+mkdir -p $HOME/challenge/unit8/2024/2/29
+cp ./answer/answer8.txt $HOME/challenge/unit8/2023/8/5/answer8.txt
